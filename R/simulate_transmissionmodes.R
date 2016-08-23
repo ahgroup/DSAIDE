963f8e16f041e47d3f14d3c70f85044a4038d729
Andreas Handel <andreas.handel@gmail.com>
2016-08-23 19:12:55 -0400

    update transmission modes

--- R/simulate_transmissionmodes.R
+++ R/simulate_transmissionmodes.R
@@ -1,5 +1,5 @@
 # This function is used in the solver function and has no independent usages
-transmissionmodeeq <- function(t, y, pars)
+transmissionmodeeq <- function(t, y, parms)
 {
   with(
     as.list(c(y,parms)), #lets us access variables and parameters stored in y and pars by name
@@ -18,7 +18,6 @@ transmissionmodeeq <- function(t, y, pars)
       list(c(dS, dI, dR, dE, dSv, dIv))
     }
   ) #close with statement
-  
 } #end function specifying the ODEs
 
   
@@ -56,7 +55,6 @@ transmissionmodeeq <- function(t, y, pars)
 #'   something nonsensical (e.g. any negative values or fractions > 1),
 #'   the code will likely abort with an error message
 #' @examples
-#' @examples
 #'   # To run the simulation with default parameters just call this function
 #'   result <- simulate_transmissionmodes()
 #'   # To choose parameter values other than the standard one, specify them e.g. like such
@@ -65,7 +63,8 @@ transmissionmodeeq <- function(t, y, pars)
 #'   plot(result[,1],result[,2],xlab='Time',ylab='Number Susceptible',type='l')
 #' @seealso The UI of the shiny app 'TransmissionModes', which is part of this package, contains more details on the model
 #' @author Andreas Handel
-#' @references We can add (a) reference(s) here..
+#' @references See e.g. Keeling and Rohani 2008 for SIR models and the
+#'   documentation for the deSolve package for details on ODE solvers
 #' @export
 
 
@@ -77,21 +76,19 @@ simulate_transmissionmodes <- function(S0 = 1e3, I0 = 1, Sv0 = 0, tmax = 120, be
   #you can change initial conditions by changing the values assigned to 
   #S0, Sv0 and I0
   #every other compartment starts at 0
-  Y0 = c(S0, I0, R0 = 0, E0 = 0, Sv0 = Sv0, Iv0 = 0);  #combine initial conditions into a vector
-
-  ############################################################
-  #specifying maximum simulation/integration time and time vector for which we want solutions
-  #you can change the maximum time for which the simulation is run by changing the value assigned to tmax
-  timevec = seq(0, tmax, 0.05); #vector of times for which solution is returned (not that internal timestep of the integrator is different)
-
+  Y0 = c(S = S0, I = I0, R = 0, E = 0, Sv = Sv0, Iv = 0);  #combine initial conditions into a vector
+  dt = min(0.1, tmax / 1000); #time step for which to get results back
+  timevec = seq(0, tmax, dt); #vector of times for which solution is returned (not that internal timestep of the integrator is different)
+  
+  
   ############################################################
   #vector of parameters which is sent to the ODE function  
-  parameters=c(beta1, beta2, beta3, beta4, lambda1, lambda2, n1, n2, gamma1, gamma2, p ,c); 
+  pars=c(beta1 = beta1, beta2 = beta2, beta3 = beta3, beta4 = beta4, lambda1 = lambda1, lambda2 = lambda2, n1 = n1, n2 = n2, gamma1 = gamma1, gamma2 = gamma2, p = p , c = c); 
 
   #this line runs the simulation, i.e. integrates the differential equations describing the infection process
   #the result is saved in the odeoutput matrix, with the 1st column the time, the 2nd, 3rd, 4th column the variables S, I, R
   #This odeoutput matrix will be re-created every time you run the code, so any previous results will be overwritten
-  odeoutput = deSolve::lsoda(Y0, timevec, func = transmissionmodeeq, parms=parameters, atol=1e-8, rtol=1e-8);
+  odeoutput = deSolve::lsoda(Y0, timevec, func = transmissionmodeeq, parms=pars, atol=1e-8, rtol=1e-8);
 
   return (odeoutput)
 }
