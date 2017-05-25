74c6150cd343432b4ee3a60a2e1c875b1f452698
SpencerDH <sd77hall@uga.edu>
2017-05-25 13:59:59 -0400

    Update simulate_multihost.R

--- inst/simulatorfunctions/simulate_multihost.R
+++ inst/simulatorfunctions/simulate_multihost.R
@@ -19,6 +19,45 @@ multieq <- function(t, y, parms)
   ) #close with statement
 } #end function specifying the ODEs
 
+#' Simulation of a compartmental infectious disease transmission model with 2 types of hosts
+#'
+#' @description  This model allows for the simulation of an ID with 2 types of hosts
+#' 
+#'
+#' @param S initial total number of hosts 
+#' @param I1 initial number of hosts infected with type 1
+#' @param I2 initial number of hosts infected with type 2
+#' @param tmax maximum simulation time, units of months
+#' @param b1 rate of transmission of type 1 pathogen to host
+#' @param b2 rate of transmission of type 2 pathogen to host
+#' @param b12 rate of transmission from infected type 2 host to susceptible type 1 host
+#' @param g1 the rate at which infected type 1 hosts recover
+#' @param g2 the rate at which infected type 2 hosts recover
+#' @param g12 the rate at which hosts infected with type 1 and 2 recover
+#' @return This function returns the simulation result as obtained from a call
+#'   to the deSolve ode solver
+#' @details A compartmental ID model with several states/compartments
+#'   is simulated as a set of ordinary differential
+#'   equations. The function returns the output from the odesolver as a matrix,
+#'   with one column per compartment/variable. The first column is time.
+#' @section Warning:
+#'   This function does not perform any error checking. So if you try to do
+#'   something nonsensical (e.g. any negative values or fractions > 1),
+#'   the code will likely abort with an error message
+#' @examples
+#'   # To run the simulation with default parameters just call this function
+#'   result <- simulate_heterogeneity()
+#'   # To choose parameter values other than the standard one, specify them e.g. like such
+#'   result <- simulate_heterogeneity(S10 = 100, S20 = 1e3,  tmax = 100)
+#'   # You should then use the simulation result returned from the function, e.g. like this:
+#'   plot(result[,1],result[,2],xlab='Time',ylab='Number Susceptible',type='l')
+#' @seealso The UI of the shiny app 'Host Heterogeneity', which is part of this package, contains more details on the model
+#' @author Andreas Handel
+#' @references See e.g. Keeling and Rohani 2008 for SIR models and the
+#'   documentation for the deSolve package for details on ODE solvers
+#' @export
+
+
 simulate_multi <- function(S = 1e3, I10 = 1, I20 = 0, I12 = 0, R1 = 0, R2 = 0, R12 = 0, tmax = 120, a = 0, b1 = 0, b2 = 0, b12 = 0, g1 = 1, g2 = 1, g12 = 1)
 {
   ############################################################
