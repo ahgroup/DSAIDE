abf4e3791f0626bb5eab079959061b529d8f6a4d
henok <henok535@uga.edu>
2018-05-31 19:54:35 -0400

    ID Control app

--- inst/shinyapps/IDControl/app.R
+++ inst/shinyapps/IDControl/app.R
@@ -11,7 +11,7 @@ refresh <- function(input, output){
   
   # This reactive takes the input data and sends it over to the simulator
   # Then it will get the results back and return it as the "res" variable
-  res <- reactive({
+  result <- reactive({
     input$submitBtn
     
     # Read all the input values from the UI
@@ -44,52 +44,136 @@ refresh <- function(input, output){
     nh = isolate(input$nh);
     mv = isolate(input$mv);
     nv = isolate(input$nv);
+    plotscale = isolate(input$plotscale)     # Change the scale of axis interactively 
     
-    # Call the ODE solver with the given parameters
-    result <- simulate_idcontrol(S0 = S0, I0 = I0, E0 = E0, Sv0 = Sv0, Iv0 = Iv0, tmax = tmax, bP = bP, bA = bA, bI = bI, bE = bE, bv = bv, bh = bh, gP = gP , gA = gA, gI = gI, pA = pA, pI = pI, c = c, f = f, d = d, w = w, mh = mh, nh = nh, mv = mv, nv = nv)
     
-    return(list(result)) #this is returned as the res variable
+    #save all results to a list for processing plots and text
+    
+    listlength = 1; #here we do all simulations in the same figure
+    result = vector("list", listlength) #create empty list of right size for results
+    
+    #shows a 'running simulation' message
+    
+    withProgress(message = 'Running Simulation', value = 0,
+                 
+      {
+          simresult <- simulate_idcontrol(S0 = S0, I0 = I0, E0 = E0, Sv0 = Sv0, Iv0 = Iv0, tmax = tmax,
+                                  bP = bP, bA = bA, bI = bI, bE = bE, bv = bv, bh = bh, gP = gP , gA = gA,
+                                 gI = gI, pA = pA, pI = pI, c = c, f = f, d = d, w = w, mh = mh, nh = nh, mv = mv, nv = nv)
+          return(list(simresult))
+                 })
+    
+
+    colnames(simresult)[1] = 'xvals' #rename time to xvals for consistent plotting
+    
+#reformat data to be in the right format for plotting
+#each plot/text output is a list entry with a data frame in form xvals, 
+#yvals, extra variables for stratifications for each plot
+    
+    dat = tidyr::gather(as.data.frame(simresult), -xvals, value = "yvals", key = "varnames")
+    
+#code variable names as factor and level them so they show up right in plot
+    
+    mylevels = unique(dat$varnames)
+    dat$varnames = factor(dat$varnames, levels = mylevels)
+    
+#data for plots and text
+#each variable listed in the varnames column will be plotted on the y-axis, with its values in yvals
+#each variable listed in varnames will also be processed to produce text
+    
+    result[[1]]$dat = dat
+    
+#Meta-information for each plot
+    
+    result[[1]]$plottype = "Lineplot"
+    result[[1]]$xlab = "Time"
+    result[[1]]$ylab = "Numbers"
+    result[[1]]$legend = "Compartments"
+    
+    result[[1]]$xscale = 'identity'
+    result[[1]]$yscale = 'identity'
+    if (plotscale == 'x' | plotscale == 'both') { result[[1]]$xscale = 'log10'}
+    if (plotscale == 'y' | plotscale == 'both') { result[[1]]$yscale = 'log10'}
+    
+    
+#set min and max for scales. If not provided ggplot will auto-set
+    
+    result[[1]]$ymin = 1e-12
+    result[[1]]$ymax = max(simresult)
+    result[[1]]$xmin = 1e-12
+    result[[1]]$xmax = tmax
+    
+#the following are for text display for each plot
+    
+    result[[1]]$maketext = TRUE #if true we want the generate_text function to process data and generate text,
+    # if 0 no result processing will occur insinde generate_text
+    result[[1]]$showtext = '' #text can be added here which will be passed through to generate_text and displayed for each plot
+    result[[1]]$finaltext = 'Numbers are rounded to 2 significant digits.' #text can be added here which will be passed through 
+    #to generate_text and displayed for each plot
+    
+    return(result)
+    
+  })      #ends inner shiny server function that runs the simulation and returns output
+  
+  
+#functions below take result saved in reactive expression result and produce output
+#to produce figures, the function generate_plot is used
+#function generate_text produces text
+#data needs to be in a specific structure for processing
+#see information for those functions to learn how data needs to look like
+#output (plots, text) is stored in reactive variable 'output'
+  
+  output$plot  <- renderPlot({
+    input$submitBtn
+    res = isolate(result())                     #list of all results that are to be turned into plots
+    generate_plots(res)                       #create plots with a non-reactive function
+  }, width = 'auto', height = 'auto'
+  )                                            #finish render-plot statement
+  
+  output$text <- renderText({
+    input$submitBtn
+    res = isolate(result())      #list of all results that are to be turned into plots
+    generate_text(res)         #create text for display with a non-reactive function
   })
   
-  #if we want certain variables plotted and reported separately, we can specify them manually as a list
-  #if nothing is specified, all variables are plotted and reported at once
-  varlist = list(c('S','P','A','I','R','D'),c('Sv','Iv'),c('E'))
-  #function that takes result saved in res and produces output
-  #output (plots, text, warnings) is stored in and modifies the global variable 'output'
-  generate_simoutput(input,output,res,varlist=varlist)
-} #ends the 'refresh' shiny server function that runs the simulation and returns output
+}             #ends the 'refresh' shiny server function that runs the simulation and returns output   
+
+
 
 #main shiny server function
 server <- function(input, output, session) {
   
-  # Waits for the Exit Button to be pressed to stop the app and return to main menu
+# Waits for the Exit Button to be pressed to stop the app and return to main menu
   observeEvent(input$exitBtn, {
     input$exitBtn
     stopApp(returnValue = 0)
   })
   
-  # This function is called to refresh the content of the Shiny App
+# This function is called to refresh the content of the Shiny App
   refresh(input, output)
   
-  # Event handler to listen for the webpage and see when it closes.
-  # Right after the window is closed, it will stop the app server and the main menu will
-  # continue asking for inputs.
+# Event handler to listen for the webpage and see when it closes.
+# Right after the window is closed, it will stop the app server and the main menu will
+# continue asking for inputs.
+  
   session$onSessionEnded(function(){
-    stopApp(returnValue = 0)
-  })
-} #ends the main shiny server function
+        stopApp(returnValue = 0)
+     })
+ }        #ends the main shiny server function
 
 
 #This is the UI part of the shiny App
+
 ui <- fluidPage(
-  includeCSS("../styles/dsaide.css"),
+   includeCSS("../styles/dsaide.css"),
   #add header and title
    
   div( includeHTML("www/header.html"), align = "center"),
   #specify name of App below, will show up in title
   h1('ID Control App', align = "center", style = "background-color:#123c66; color:#fff"),
   
-  #section to add buttons
+#section to add buttons
+
   fluidRow(
     column(6,
            actionButton("submitBtn", "Run Simulation", class="submitbutton")  
@@ -102,8 +186,9 @@ ui <- fluidPage(
   
   tags$hr(),
   
-  ################################
+################################
   #Split screen with input on left, output on right
+
   fluidRow(
     #all the inputs in here
     column(6,
@@ -203,7 +288,14 @@ ui <- fluidPage(
              column(6,
                     numericInput("nv", "death rate of vectors (nv)", min = 0, max = 30, value = 0, step = 0.1  )
              )
-           ) #close fluidRow structure for input
+           ), #close fluidRow structure for input
+           
+           fluidRow(
+             column(4, align = "left",
+                    selectInput("plotscale", "Log-scale for plot:",c("none" = "none", 'x-axis' = "x", 'y-axis' = "y", 'both axes' = "both"))
+             )
+             
+           )  
            
     ), #end sidebar column for inputs
     #all the outcomes here
