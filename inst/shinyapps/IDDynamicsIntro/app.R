a09b7aba02ca32ed1e01fa2aaf43a731c45fbde3
henok <henok535@uga.edu>
2018-06-09 10:40:53 -0400

    ID DynamicInro App

--- inst/shinyapps/IDDynamicsIntro/app.R
+++ inst/shinyapps/IDDynamicsIntro/app.R
@@ -7,36 +7,116 @@
 
 #the server-side function with the main functionality
 #this function is wrapped inside the shiny server function below to allow return to main menu when window is closed
-refresh <- function(input, output){
+
+ refresh <- function(input, output){
   
   # This reactive takes the input data and sends it over to the simulator
   # Then it will get the results back and return it as the "res" variable
-  res <- reactive({
+   
+  result <- reactive({
     input$submitBtn
     
     
-    # Read all the input values from the UI
+# Read all the input values from the UI
+    
     S0 = isolate(input$S0);
     I0 = isolate(input$I0);
     b = isolate(input$b);
     g = isolate(input$g);
     tmax = isolate(input$tmax);
-
-    # Call the ODE solver with the given parameters
-    result <- simulate_introduction(S0 = S0, I0 = I0, g = g, b = b, tmax = tmax)
+    plotscale = isolate(input$plotscale)     # Change the scale of axis interactively
     
-    return(list(result)) #this is returned as the res variable
+#save all results to a list for processing plots and text
+    
+    listlength = 1;       #here we do all simulations in the same figure
+    result = vector("list", listlength) #create empty list of right size for results
+    
+#shows a 'running simulation' message
+    
+    withProgress(message = 'Running Simulation', value = 0,
+                 {
+                   simresult <- simulate_introduction(S0 = S0, I0 = I0, g = g, b = b, tmax = tmax)
+                 })
+    colnames(simresult) = c('xvals','S','I','R')
+    
+#reformat data to be in the right format for plotting
+#each plot/text output is a list entry with a data frame in form xvals, yvals, extra variables for stratifications for each plot
+    
+    dat = tidyr::gather(as.data.frame(simresult), -xvals, value = "yvals", key = "varnames")
+    
+    #code variable names as factor and level them so they show up right in plot
+    
+    mylevels = unique(dat$varnames)
+    dat$varnames = factor(dat$varnames, levels = mylevels)
+    
+#data for plots and text
+#each variable listed in the varnames column will be plotted on the y-axis, with its values in yvals
+#each variable listed in varnames will also be processed to produce text
+    
+    result[[1]]$dat = dat
+    
+#Meta-information for each plot
+    
+    result[[1]]$plottype = "Lineplot"
+    result[[1]]$xlab = "Time"
+    result[[1]]$ylab = "Numbers"
+    result[[1]]$legend = "Compartments"
+    
+    result[[1]]$xscale = 'identity'
+    result[[1]]$yscale = 'identity'
+    if (plotscale == 'x' | plotscale == 'both') { result[[1]]$xscale = 'log10'}
+    if (plotscale == 'y' | plotscale == 'both') { result[[1]]$yscale = 'log10'}
+    
+    
+#set min and max for scales. If not provided ggplot will auto-set
+    
+    result[[1]]$ymin = 1e-12
+    result[[1]]$ymax = max(simresult)
+    result[[1]]$xmin = 1e-12
+    result[[1]]$xmax = tmax
+    
+#the following are for text display for each plot
+    
+    result[[1]]$maketext = TRUE #if true we want the generate_text function to process data and generate text, if 0 no result processing will occur insinde generate_text
+    result[[1]]$showtext = '' #text can be added here which will be passed through to generate_text and displayed for each plot
+    result[[1]]$finaltext = 'Numbers are rounded to 2 significant digits.' #text can be added here which will be passed through to generate_text and displayed for each plot
+    
+    return(result)
+    
+  })   #ends inner shiny server function that runs the simulation and returns output
+  
+  
+#functions below take result saved in reactive expression result and produce output
+#to produce figures, the function generate_plot is used
+#function generate_text produces text
+#data needs to be in a specific structure for processing
+#see information for those functions to learn how data needs to look like
+#output (plots, text) is stored in reactive variable 'output'
+  
+  output$plot  <- renderPlot({
+    input$submitBtn
+    res=isolate(result())   #list of all results that are to be turned into plots
+    generate_plots(res)      #create plots with a non-reactive function
+  }, width = 'auto', height = 'auto'
+  )    #finish render-plot statement
+  
+  output$text <- renderText({
+    input$submitBtn
+    res=isolate(result())        #list of all results that are to be turned into plots
+    generate_text(res)     #create text for display with a non-reactive function
   })
   
-  #function that takes result saved in res and produces output
-  #output (plots, text, warnings) is stored in and modifies the global variable 'output'
-  generate_simoutput(input,output,res)
-} #ends the 'refresh' shiny server function that runs the simulation and returns output
+  
+  
+} #ends the 'refresh' shiny server function that runs the simulation and returns output   
+
 
 #main shiny server function
+ 
 server <- function(input, output, session) {
   
   # Waits for the Exit Button to be pressed to stop the app and return to main menu
+  
   observeEvent(input$exitBtn, {
     input$exitBtn
     stopApp(returnValue = 0)
@@ -48,13 +128,15 @@ server <- function(input, output, session) {
   # Event handler to listen for the webpage and see when it closes.
   # Right after the window is closed, it will stop the app server and the main menu will
   # continue asking for inputs.
+  
   session$onSessionEnded(function(){
     stopApp(returnValue = 0)
   })
-} #ends the main shiny server function
+}       #ends the main shiny server function
 
 
 #This is the UI part of the shiny App
+
 ui <- fluidPage(
   includeCSS("../styles/dsaide.css"),
   #add header and title
@@ -66,6 +148,7 @@ ui <- fluidPage(
   h1('ID Dynamics Introduction App', align = "center", style = "background-color:#123c66; color:#fff"),
   
   #section to add buttons
+  
   fluidRow(
     column(6,
            actionButton("submitBtn", "Run Simulation", class="submitbutton")  
@@ -78,8 +161,9 @@ ui <- fluidPage(
   
   tags$hr(),
   
-  ################################
-  #Split screen with input on left, output on right
+################################
+#Split screen with input on left, output on right
+  
   fluidRow(
     #all the inputs in here
     column(6,
@@ -87,32 +171,35 @@ ui <- fluidPage(
            # Inputs section
            h2('Simulation Settings'),
            fluidRow( class = 'myrow',
-             column(4,
-                    numericInput("S0", "Initial number of susceptible hosts (S0)", min = 500, max = 5000, value = 1000, step = 500)
-             ),
-             column(4,
-                    numericInput("I0", "Initial number of infected hosts (I0)", min = 0, max = 100, value = 0, step = 1)
-             ),
-             column(4,
-                    numericInput("tmax", "Maximum simulation time (tmax)", min = 10, max = 1000, value = 300, step = 10)
-             ),
-             align = "center"
+                     column(4,
+                            numericInput("S0", "Initial number of susceptible hosts (S0)", min = 500, max = 5000, value = 1000, step = 500)
+                     ),
+                     column(4,
+                            numericInput("I0", "Initial number of infected hosts (I0)", min = 0, max = 100, value = 0, step = 1)
+                     ),
+                     column(4,
+                            numericInput("tmax", "Maximum simulation time (tmax)", min = 10, max = 1000, value = 300, step = 10)
+                     ),
+                     align = "center"
            ), #close fluidRow structure for input
            
            fluidRow(class = 'myrow',
-             column(6,
-                    numericInput("b", "Rate of transmission (b)", min = 0, max = 0.01, value = 0, step = 0.0001 )
-             ),
-             column(6,
-                    numericInput("g", "Rate at which a host leaves the infectious compartment (g)", min = 0, max = 2, value = 0.5, step = 0.1)
-             ),
-             align = "center" 
+                    column(4,
+                           numericInput("b", "Rate of transmission (b)", min = 0, max = 0.01, value = 0, step = 0.0001 )
+                    ),
+                    column(4,
+                           numericInput("g", "Rate at which a host leaves the infectious compartment (g)", min = 0, max = 2, value = 0.5, step = 0.1)
+                    ),
+                    column(4,
+                           selectInput("plotscale", "Log-scale for plot:",c("none" = "none", 'x-axis' = "x", 'y-axis' = "y", 'both axes' = "both")) 
+                    ),
+                    align = "center" 
            ) #close fluidRow structure for input
            
            
     ), #end sidebar column for inputs
     
-    #all the outcomes here
+#all the outcomes here
     column(6,
            
            #################################
@@ -129,13 +216,13 @@ ui <- fluidPage(
                                 }")),
            tags$hr()
            
-           ) #end main panel column with outcomes
+    ) #end main panel column with outcomes
   ), #end layout with side and main panel
   
-  #################################
-  #Instructions section at bottom as tabs
+#################################
+#Instructions section at bottom as tabs
   h2('Instructions'),
-  #use external function to generate all tabs with instruction content
+#use external function to generate all tabs with instruction content
   do.call(tabsetPanel,generate_instruction_tabs()),
   div(includeHTML("www/footer.html"), align="center", style="font-size:small") #footer
   
