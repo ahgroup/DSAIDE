159fa0e87f19a3715510b0fe6dcede2f7406a51b
henok <henok535@uga.edu>
2018-06-12 20:34:54 -0400

    Vector Transmission App Updated

--- inst/shinyapps/VectorTransmission/app.R
+++ inst/shinyapps/VectorTransmission/app.R
@@ -11,7 +11,7 @@ refresh <- function(input, output){
   
   # This reactive takes the input data and sends it over to the simulator
   # Then it will get the results back and return it as the "res" variable
-  res <- reactive({
+  result <- reactive({
     input$submitBtn
     
     # Read all the input values from the UI
@@ -27,21 +27,108 @@ refresh <- function(input, output){
     n  = isolate(input$n);
     g  = isolate(input$g);
     w  = isolate(input$w);
+    plotscale = isolate(input$plotscale)
     
+    #save all results to a list for processing plots and text
+    listlength = 2; #here we do all simulations in the same figure
+    result = vector("list", listlength) #create empty list of right size for results
     
     # Call the ODE solver with the given parameters
-    result <- simulate_vectortransmission(Sh0 = Sh0, Ih0 = Ih0, Sv0 = Sv0, Iv0 = Iv0, tmax = tmax, b1 = b1, b2 = b2, m = m, n = n, g = g, w = w)
     
-    return(list(result)) #this is returned as the res variable
+    withProgress(message = 'Running Simulation', value = 0, {
+      
+    simresult <- simulate_vectortransmission(Sh0 = Sh0, Ih0 = Ih0, Sv0 = Sv0, Iv0 = Iv0, tmax = tmax, b1 = b1,
+                                          b2 = b2, m = m, n = n, g = g, w = w)
+        
   })
+    
+#rename time to xvals for consistent plotting
+    
+   colnames(simresult) = c('xvals',"Sh","Ih","Rh","Sv","Iv")
+    
+# reformat data to be in the right format for plotting 
+# dat1 store the input used for plot 1
+# dat2 store the input used for plot 2
+   
+    dat1 = tidyr::gather(as.data.frame(simresult[,c(1,2:4)]), -xvals, value = "yvals", key = "varnames")
+    dat2 = tidyr::gather(as.data.frame(simresult[,c(1,5:6)]), -xvals, value = "yvals", key = "varnames")
+    
+#code variable names as factor and level them so they show up right in plot   
+    mylevels1 = unique(dat1$varnames)
+    dat1$varnames = factor(dat1$varnames, levels = mylevels1)
+    
+    mylevels2 = unique(dat2$varnames)
+    dat2$varnames = factor(dat2$varnames, levels = mylevels2)
+    
+    #data for plots and text
+    #each variable listed in the varnames column will be plotted on the y-axis, with its values in yvals
+    #each variable listed in varnames will also be processed to produce text
+    
+    result[[1]]$dat = dat1
+    result[[2]]$dat = dat2
+    
+    
+    for (i in 1 :2) {
+     
+#Meta-information for each plot
+      
+      result[[i]]$plottype = "Lineplot"
+      result[[i]]$xlab = "Time"
+      result[[i]]$ylab = "Numbers"
+      result[[i]]$legend = "Compartments"
+      
+               result[[i]]$xscale = 'identity'
+               result[[i]]$yscale = 'identity'
+             if (plotscale == 'x' | plotscale == 'both') { result[[i]]$xscale = 'log10'}
+             if (plotscale == 'y' | plotscale == 'both') { result[[i]]$yscale = 'log10'}
+      
+      
+#set min and max for scales. If not provided ggplot will auto-set
+      
+             result[[i]]$ymin = 1e-12
+             result[[i]]$ymax = max(simresult)
+             result[[i]]$xmin = 1e-12
+             result[[i]]$xmax = tmax
+      
+#the following are for text display for each plot
+      
+      result[[i]]$maketext = TRUE #if true we want the generate_text function to process data and generate  
+                                  # text,if 0 no result processing will occur insinde generate_text
+      result[[i]]$showtext = ''  #text can be added here which will be passed through to generate_text
+                                 #and displayed for each plot
+      result[[i]]$finaltext = 'Numbers are rounded to 2 significant digits.' #text can be added here which 
+                                      #will be passed through to generate_text and displayed for each plot
+      
+  }
+
+    return(result) 
+    
+  })
+ 
+#functions below take result saved in reactive expression result and produce output
+#to produce figures, the function generate_plot is used
+#function generate_text produces text
+#data needs to be in a specific structure for processing
+#see information for those functions to learn how data needs to look like
+#output (plots, text) is stored in reactive variable 'output'
+    
+  output$plot  <- renderPlot({
+         input$submitBtn
+         res=isolate(result())             #list of all results that are to be turned into plots
+        generate_plots(res)              #create plots with a non-reactive function
+    }, width = 'auto', height = 'auto'
+  )                                       #finish render-plot statement
+    
+  output$text <- renderText({
+      input$submitBtn
+      res=isolate(result())     #list of all results that are to be turned into plots
+      generate_text(res)         #create text for display with a non-reactive function
+   })
+    
   
-  #if we want certain variables plotted and reported separately, we can specify them manually as a list
-  #if nothing is specified, all variables are plotted and reported at once
-  varlist = list(c("Sh","Ih","Rh"), c("Sv",'Iv'))
-  #function that takes result saved in res and produces output
-  #output (plots, text, warnings) is stored in and modifies the global variable 'output'
-  generate_simoutput(input,output,res,varlist=varlist)
-} #ends the 'refresh' shiny server function that runs the simulation and returns output
+  
+     
+ } #ends the 'refresh' shiny server function that runs the simulation and returns output
 
 #main shiny server function
 server <- function(input, output, session) {
@@ -132,12 +219,15 @@ ui <- fluidPage(
            ), #close fluidRow structure for input
            
            fluidRow(
-               column(6,
+               column(4,
                     numericInput("m", "Rate of new vector births (m)", min = 0, max = 10000, value = 0, step = 100)
              ),
-             column(6,
+             column(4,
                     numericInput("n", "Vector death rate (n)", min = 0, max = 2, value = 0, step = 0.01 )
              ),
+             column(4,
+                    selectInput("plotscale", "Log-scale for plot:",c("none" = "none", 'x-axis' = "x", 'y-axis' = "y", 'both axes' = "both"))
+              ), 
              align = "center"
            ) #close fluidRow structure for input
     ), #end sidebar column for inputs
