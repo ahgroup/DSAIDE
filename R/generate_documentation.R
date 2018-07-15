201376680aeeafc36ff2b6e8fb5b1a94fefd30b4
Andreas Handel <andreas.handel@gmail.com>
2018-07-15 11:53:45 -0400

    more doc edits

--- R/generate_documentation.R
+++ R/generate_documentation.R
@@ -11,65 +11,24 @@
 
 generate_documentation <- function()
 {
-    #it is better if a developer creates the html by knitting
-    #their Rmd file instead of producing it on the fly
-    #therefore this code bit is currently disabled,
-    #but left in here for future reference
-    #first, check if no html file of documentation is available
-    #or if it is outdated. In that case, use Rmd file to create
-    #html file
-    #for this to work, there can only be one Rmd and 0 or 1 html files
-    #in the folder where the app is
-    # htmlfile = list.files(path = currentdir, pattern = "\\.html$")
-    # rmdfile = list.files(path = currentdir, pattern = "\\.Rmd$")
-    # if (length(htmlfile)==0) #html file doesn't exist, create
-    # {
-    #   rmarkdown::render(rmdfile)
-    # }      
-    # if (length(htmlfile)>0) #html file exists, check date
-    # {
-    #   htmldate <- file.info(htmlfile)$mtime
-    #   rmddate <- file.info(rmdfile)$mtime
-    #   if ((rmddate-htmldate)>0) #if rmd file is newer, recreate html
-    #   {
-    #     rmarkdown::render(rmdfile)
-    #   }
-    # }
-
-    #now take HTML file and split it into components for each tab
+    #take HTML file and split it into components for each tab
     currentdir = getwd() 
-    htmlfile = list.files(path = currentdir, pattern = "\\.html$")
-    
-    htmldoc = read_html(htmlfile)
-    #test = rvest::html_nodes(htmldoc, "h2") 
-    #titles =  html_text(test)  
-    #browser()
-    #test = rvest::html_nodes(htmldoc, 'div# :contains(shinytab)')
-    html.parse.shinytabs = rvest::html_nodes(htmldoc, xpath = "//div[@id[starts-with(., 'shinytab')]]" )
-    htmlcontent = html_text(html.parse.shinytabs)
-    # Read and parse raw HTML file
-    #html.raw <- XML::htmlTreeParse(htmlfile, useInternalNodes = TRUE)
-    
-    # Find all the DIV tags within the html structure which has shinytab* ID
-    #html.parse.shinytabs = XML::getNodeSet(html.raw, "//div[@id[starts-with(., 'shinytab')]]") # xmlValue or saveXML
-    #browser()
-    #list with content for tabs
-    #tablist = NULL
+    tablist = NULL
     tabtitles = c('Overview','The Model','What to do','Further Information')    
-    tablist = shiny::tabPanel(htmlcontent)
+
+    htmlfile = list.files(path = currentdir, pattern = "\\.html$")
+    #htmldoc = xml2::read_html(htmlfile)
+    #shinyblocks = xml2::xml_find_all(htmldoc,xpath = "//div[@id[starts-with(., 'shinytab')]]")
     
-    browser()
-    # Now loop over the shinytab sections and save them as a list
-    #for (i in seq(length(html.parse.shinytabs)))
-    #{
-      
-    #  subDoc <- XML::xmlDoc(html.parse.shinytabs[[i]])
-      # Get the content of the shinytab
-    #  content <- XML::xpathApply(subDoc, "//div[@id[starts-with(., 'shinytab')]]", XML::saveXML)
-    #  htmlcontent = shiny::HTML(content[[1]])
-      #save title and content as shiny tabPanel
-    #  tablist[[i]] = shiny::tabPanel(tabtitles[i], htmlcontent, icon = NULL) 
-    #}
+    html.raw <- XML::htmlTreeParse(htmlfile, useInternalNodes = TRUE)
+    shinyblocks = XML::getNodeSet(html.raw, "//div[@id[starts-with(., 'shinytab')]]") 
+    for (i in 1:4)
+    {
+      subDoc <- XML::xmlDoc(shinyblocks[[i]])
+      content <- XML::xpathApply(subDoc, "//div[@id[starts-with(., 'shinytab')]]", saveXML) 
+      htmlcontent = shiny::HTML(content[[1]])
+      tablist[[i]] = shiny::tabPanel(tabtitles[i], htmlcontent, icon = NULL) 
+    }
     return(tablist)
 }
 
