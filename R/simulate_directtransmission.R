2ff2a93321743128b3e4207653e6aa596255d43e
Spencer Hall <sd77hall@uga.edu>
2018-07-24 12:34:50 -0400

    Update simulate_directtransmission.R

--- R/simulate_directtransmission.R
+++ R/simulate_directtransmission.R
@@ -82,7 +82,7 @@ simulate_directtransmission <- function(S0 = 1e3, I0 = 1, tmax = 120, scenario =
   #This odeoutput matrix will be re-created every time you run the code, so any previous results will be overwritten
   odeoutput = deSolve::lsoda(Y0, timevec, func = directtransmissioneq, parms=pars, atol=1e-12, rtol=1e-12);
   
-  colnames(odeoutput) = c('xvals','S','I','R')
+  colnames(odeoutput) = c('Time','S','I','R')
   
   result = list()
   result$ts <- as.data.frame(odeoutput)
