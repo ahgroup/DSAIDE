f935843fcf11981aaac61b62d986bc0a39b9cdba
Spencer Hall <sd77hall@uga.edu>
2018-07-24 12:33:06 -0400

    Update app.R

--- inst/shinyapps/DirectTransmission/app.R
+++ inst/shinyapps/DirectTransmission/app.R
@@ -43,7 +43,7 @@ refresh <- function(input, output){
     #reformat data to be in the right format for plotting
     #each plot/text output is a list entry with a data frame in form xvals, yvals, extra variables for stratifications for each plot
     
-    dat = tidyr::gather(simresult$ts, -xvals, value = "yvals", key = "varnames")
+    # dat = tidyr::gather(simresult$ts, -xvals, value = "yvals", key = "varnames")
     
     #code variable names as factor and level them so they show up right in plot
     
