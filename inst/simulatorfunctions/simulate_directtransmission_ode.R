d7f92a0c12a1fb10b5cb1df73a84f5831cead1f8
Brian McKay <bmckay52@uga.edu>
2020-08-13 10:47:24 -0400

    Direct Transmission
    Updated the notation. Added a new record.

--- inst/simulatorfunctions/simulate_directtransmission_ode.R
+++ inst/simulatorfunctions/simulate_directtransmission_ode.R
@@ -7,8 +7,8 @@
 #' @param bd : rate of transmission for density-dependent transmission : numeric
 #' @param bf : rate of transmission for frequency-dependent transmission : numeric
 #' @param A : the size of the area in which the hosts are assumed to reside/interact : numeric
-#' @param m : the rate of births : numeric
-#' @param n : the rate of natural deaths : numeric
+#' @param n : the rate of births : numeric
+#' @param m : the rate of natural deaths : numeric
 #' @param g : the rate at which infected hosts recover : numeric
 #' @param w : the rate of waning immunity : numeric
 #' @param scenario : choice between density dependent (=1) and frequency dependent (=2) transmission : numeric
@@ -40,10 +40,10 @@
 
 
 
-simulate_directtransmission_ode <- function(S = 1e3, I = 1, bd = 0.01, bf = 0, A = 1, m = 0, n = 0, g = 0.1, w = 0, scenario = 1, tmax = 120)
+simulate_directtransmission_ode <- function(S = 1e3, I = 1, bd = 0.01, bf = 0, A = 1, n = 0, m = 0, g = 0.1, w = 0, scenario = 1, tmax = 120)
 {
 
-  
+
   ############################################################
   # This function is used in the solver function and has no independent usages
   directtransmissioneq <- function(t, y, parms)
@@ -51,44 +51,44 @@ simulate_directtransmission_ode <- function(S = 1e3, I = 1, bd = 0.01, bf = 0, A
     with(
       as.list(c(y,parms)), #lets us access variables and parameters stored in y and pars by name
       {
-        
-        
+
+
         #force of infection for different scenarios: 1 = density-dependent, 2 = frequency-dependent
         N=S+I+R
         if (scenario==1) { f=bd*I/A;}
         if (scenario==2) { f=bf*I/N;}
-        
+
         #the ordinary differential equations - includes birth-death and waning immunity
-        dS = m - n*S - f*S + w*R; #susceptibles 
+        dS = m - n*S - f*S + w*R; #susceptibles
         dI = f*S - g*I - n*I; #infected/infectious
         dR = g*I -n*R - w*R; #recovered
-        
+
         list(c(dS, dI, dR))
       }
     ) #close with statement
   } #end function specifying the ODEs
-  
-  
-  
+
+
+
   ############################################################
   #Start main function code
   ############################################################
   Y0 = c(S = S, I = I, R = 0);  #combine initial conditions into a vector
   dt = min(0.1, tmax / 1000); #time step for which to get results back
   timevec = seq(0, tmax, dt); #vector of times for which solution is returned (not that internal timestep of the integrator is different)
-  
-  
+
+
   ############################################################
-  #vector of parameters which is sent to the ODE function  
-  pars=c(tmax = tmax, bd = bd, bf = bf, A = A, m = m, n = n, g = g, w = w, scenario = scenario); 
+  #vector of parameters which is sent to the ODE function
+  pars=c(tmax = tmax, bd = bd, bf = bf, A = A, n = n, m = m, g = g, w = w, scenario = scenario);
 
   #this line runs the simulation, i.e. integrates the differential equations describing the infection process
   #the result is saved in the odeoutput matrix, with the 1st column the time, the remaining columns the model variables
   odeoutput = deSolve::lsoda(Y0, timevec, func = directtransmissioneq, parms=pars, atol=1e-12, rtol=1e-12);
-  
+
   result = list()
   result$ts <- as.data.frame(odeoutput)
-  
+
   return(result)
   # return(odeoutput)
 }
