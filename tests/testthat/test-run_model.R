94ea2d9bb7ba4bfb2b278b7d275df1356dbccbdf
Andreas Handel <andreas.handel@gmail.com>
2020-09-01 18:18:39 -0400

    updated environmental and vector apps

--- tests/testthat/test-run_model.R
+++ tests/testthat/test-run_model.R
@@ -4,7 +4,7 @@ context("test-run_model.R")
 test_that("run_model correctly runs different models",
 {
             tfinal = 120
-            modelsettings =  list(S = 1000, I = 10, R = 0 , b = 1e-3, g = 0.5, tstart = 0, tfinal = tfinal, dt = 0.1, modeltype = "_ode_", plotscale = 'y', nplots = 1)
+            modelsettings =  list(S = 1000, I = 10, R = 0 , n = 0, b = 1e-3, g = 0.5, w = 0, m = 0, tstart = 0, tfinal = tfinal, dt = 0.1, modeltype = "_ode_", plotscale = 'y', nplots = 1)
             modelsettings$simfunction = 'simulate_SIRSd_model_ode'
             result = run_model(modelsettings)
             #check that simulation ran until max time
@@ -15,7 +15,7 @@ test_that("run_model correctly runs different models",
             expect_is(generate_plotly(result), "plotly" )
             testthat::  expect_is(generate_text(result), "html" )
 
-            modelsettings =  list(S = 1000, I = 10, R = 0 , b = 1e-3, g = 0.5, m = 0, n = 0, tmax = 100, rngseed = 123, modeltype = "_stochastic_", plotscale = 'y', nplots = 1,  nreps = 1)
+            modelsettings =  list(S = 1000, I = 10, R = 0 , n = 0, b = 1e-3, g = 0.5, w = 0, m = 0, tstart = 0, tfinal = tfinal, rngseed = 123, modeltype = "_stochastic_", plotscale = 'y', nplots = 1,  nreps = 1)
             modelsettings$simfunction = 'simulate_SIRSd_model_stochastic'
             result = run_model(modelsettings)
             #check that simulation returned specific value of susceptible at end
@@ -76,12 +76,12 @@ test_that("run_model correctly runs different models",
             expect_is(generate_plotly(result), "plotly" )
             testthat::  expect_is(generate_text(result), "html" )
 
-            modelsettings =  list(Sh = 1000, Ih = 10, Rh = 0 , b1 = 1e-3, g = 0.5, m = 0, n = 0, tmax = 100, modeltype = "_ode_",  nplots = 1)
+            modelsettings =  list(Sh = 1000, Ih = 10, Rh = 0 , Sv = 1000, Iv = 1, b1 = 0.002, b2 = 0.002, g = 1, w = 0, m = 2, n = 2000, tstart  = 0, tfinal = 100, dt = 0.1, modeltype = "_ode_",  nplots = 1)
             modelsettings$simfunction = 'simulate_Vector_transmission_model_ode'
             result = run_model(modelsettings)
             #check that simulation returned specific value of susceptible at end
             Sfinal = round(min(dplyr::filter(result[[1]]$dat, varnames == "Sh")$yvals))
-            expect_equal(Sfinal, 74)
+            expect_equal(Sfinal, 231)
             expect_is(generate_ggplot(result), "ggplot" )
             expect_is(generate_plotly(result), "plotly" )
             testthat::  expect_is(generate_text(result), "html" )
