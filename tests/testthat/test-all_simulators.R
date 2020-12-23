c988cba814e9b15af4e86e8c8f011cecfd6d24d5
Andreas Handel <andreas.handel@gmail.com>
2020-12-23 14:10:36 -0500

    add checks

---
+++ tests/testthat/test-all_simulators.R
@@ -0,0 +1,25 @@
+context("test-all_simulators.R")
+
+
+test_that("all simulator functions run correctly when called directly",
+{
+
+            sim = simulate_Characteristics_of_ID_ode()
+            expect_type(sim,"list")
+
+            sim = simulate_Environmental_Transmission_model_ode()
+            expect_type(sim,"list")
+
+            sim = simulate_directtransmission_ode()
+            expect_type(sim,"list")
+
+            sim = simulate_idcontrolmultigroup_ode()
+            expect_type(sim,"list")
+
+            sim = simulate_idvaccine_ode()
+            expect_type(sim,"list")
+
+            sim = simulate_idsurveillance_ode()
+            expect_type(sim,"list")
+
+})
