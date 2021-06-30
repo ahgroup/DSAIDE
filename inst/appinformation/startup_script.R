1bdf04d3489d710aaa5244567b1dfd6f397442fb
Andreas Handel <andreas.handel@gmail.com>
2021-06-30 19:13:06 -0400

    ed

--- inst/appinformation/startup_script.R
+++ inst/appinformation/startup_script.R
@@ -20,7 +20,6 @@ appdocdir = "appinformation"
 # get_settings.R reads settings from apptable.tsv
 # write_tasktext.R writes the text for all tasks into the Rmd/Html file
 filenames = c("get_settings.R","write_tasktext.R")
-#filenames = c("fill_tasktable.R","get_settings.R","make_tasktable.R","save_tasktable.R","write_tasktext.R")
 files_to_source = paste(system.file(helperdir,package = packagename),filenames,sep="/")
 #source these files
 sapply(files_to_source, source)
